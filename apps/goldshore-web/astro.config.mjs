7e45e72ed8638da031b499933b28884de727df5b
Bobby Rosenberg <34346749+marzton@users.noreply.github.com>
2025-11-09 17:30:45 -0500

    Revert "Bootstrap pnpm Monorepo Foundation (#2)"
    This reverts commit 46dc40b529369c91c2e7fc6c86e22b8b091dac63.

--- apps/goldshore-web/astro.config.mjs
+++ apps/goldshore-web/astro.config.mjs
@@ -2,11 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    vite: {
-        ssr: {
-            // Include shared packages in the SSR build
-            noExternal: ['@goldshore/auth', '@goldshore/ui', '@goldshore/utils'],
-        },
-    },
-});
+export default defineConfig({});
